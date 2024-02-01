366df6d4ed63e06e744c0284e3c8cf95c1d49f22
ashhass <se.ayda.sultan@gmail.com>
2024-02-01 18:46:56 +0300

    data loading process done

--- trainer.py
+++ trainer.py
@@ -16,36 +16,35 @@ class Trainer():
         return data
 
 
-    def split_data(self, file):
-        input = self.load_data(file) 
-        size = int(0.9 * (len(input)))
-        train_data, val_data = input[ : size], input[size : ]
-        return train_data, val_data
+    def split_data(self, data):
+        input = self.tokenize_data(self.load_data(data))['input_ids']
+        size = int(0.9 * (len(input))) 
+        train_data, val_data = input[ : size], input[size : ] 
+        return torch.tensor(train_data), torch.tensor(val_data)
 
 
-    def prepare_data(self, data):
-        train_data, _ = self.split_data(data)
-        x, y = train_data[ : self.context_length], train_data[1 : self.context_length + 1]
+    def tokenize_data(self, data):
+        x, y = data[ : self.context_length], data[1 : self.context_length + 1]
         
         for size in range(self.context_length):
-            context = train_data[: size + 1]
-            target = train_data[size] 
+            context = data[: size + 1] 
+            target = data[size] 
 
-        return self.tokenizer(train_data) 
+        return self.tokenizer(data) 
     
     def load_batch(self, split, file):
+        
         train_data, val_data = self.split_data(file)
         data = train_data if split == 'train' else val_data
 
         ix = torch.randint(len(data) - self.context_length, (self.batch_size,))
-        x = torch.stack([data[ : self.context_length + i] for i in ix])
+        x = torch.stack([data[i : self.context_length + i] for i in ix])
         y = torch.stack([data[i+1 : self.context_length + i + 1] for i in ix])
-        print(x, y) 
-
+        print(x, y)
 
     def trainer(self):
         return 
         
 
 trainer = Trainer(context_length = 10, batch_size = 8)
-trainer.load_batch('train', 'input.txt') 
+trainer.load_batch('train', 'input.txt')  
