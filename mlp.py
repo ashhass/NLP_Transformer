04e04ecaef3bb1dbc5a946513d2a427dc8333834
ashhass <se.ayda.sultan@gmail.com>
2024-02-07 15:39:12 +0300

    layer normalization added

--- mlp.py
+++ mlp.py
@@ -20,12 +20,14 @@ class Block(nn.Module):
   def __init__(self, n_embd, context_length, num_heads):
     super().__init__()
     head_size = n_embd // num_heads
+    self.ln1 = nn.LayerNorm(n_embd)
+    self.ln2 = nn.LayerNorm(n_embd)
     self.attention = MultiHeadAttention(n_embd, context_length, num_heads, head_size)
     self.MLP = MLP(n_embd)
 
 
   def forward(self, x):
-    attn = x + self.attention(x)
-    out = attn + self.MLP(attn)
+    attn = x + self.attention(self.ln1(x))
+    out = attn + self.MLP(self.ln2(attn))
 
     return out
